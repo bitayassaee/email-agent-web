d50f9cf800f162417e5192b422caccf1b57c1a83
bita <bitays200@gmail.com>
2025-10-04 17:40:09 +0330

    feat: handel form submisson and display user input

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from dis import Instruction
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    generated_email = None
+    if request.method == "POST":
+        Instructions=request.form.get("instructions")
+        generated_email=f"you wrote: {Instructions}"
+    return render_template("index.html", generated_email=generated_email)
+
 
 
 if __name__ == "__main__":
